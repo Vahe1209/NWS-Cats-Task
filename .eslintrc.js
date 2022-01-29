4137d22da56a07bc23a944e5a25119117d566254
Vahe1209 <Vahe1209@gmail.com>
2022-01-29 12:15:21 +0400

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    jest: true,
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/prop-types': 0,
+    'react/require-default-props': 0,
+    'react/forbid-prop-types': 0,
+    'import/no-named-as-default': 0,
+    'import/no-named-as-default-member': 0,
+    'import/no-unresolved': 0,
+    'react/jsx-props-no-spreading': 0,
+    'no-unused-expressions': 0,
+    'consistent-return': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'import/prefer-default-export': 0,
+    'no-restricted-syntax': 0,
+    quotes: [2, 'single', { avoidEscape: true }],
+    'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: ['draft'] }],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+}
